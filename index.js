201b4ca9c7f7fa3d059d0784709051eb0fff9711
gloria <gloria.gao115@gmail.com>
2023-03-01 15:29:33 +1100

    done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  dodger.style.left = '0px'
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+function moveDodgerRight() {
+  dodger.style.left = "360px";
+  // dodger.style.right = "0px";
+
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
